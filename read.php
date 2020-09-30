817eb2a47fb6e9eec008db88e8df37e92b781da7
Md Maksud Hossain <maksud.1969@gmail.com>
2020-09-30 07:43:20 +0600

    Reading Added.

---
+++ read.php
@@ -0,0 +1,50 @@
+<?php 
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+
+  include_once 'Database.php';
+  include_once 'Products.php';
+
+  // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
+
+  // Instantiate blog post object
+  $post = new Products($db);
+
+  // Blog post query
+  $result = $post->read();
+  // Get row count
+  $num = $result->rowCount();
+
+  // Check if any posts
+  if($num > 0) {
+    // Post array
+    $posts_arr = array();
+    // $posts_arr['data'] = array();
+
+    while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+      extract($row);
+
+      $post_item = array(
+        'id' => $id,
+        'Name' => $Name,
+        'Price' => $Price,
+        'Time' => $Time        
+      );
+
+      // Push to "data"
+      array_push($posts_arr, $post_item);
+      // array_push($posts_arr['data'], $post_item);
+    }
+
+    // Turn to JSON & output
+    echo json_encode($posts_arr);
+
+  } else {
+    // No Posts
+    echo json_encode(
+      array('message' => 'No Posts Found')
+    );
+  }
