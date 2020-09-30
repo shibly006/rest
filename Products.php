1ea35182738684268dfa5ce3c8a56b95575c089d
Md Maksud Hossain <maksud.1969@gmail.com>
2020-09-30 07:27:39 +0600

    First Commit

---
+++ Products.php
@@ -0,0 +1,36 @@
+<?php 
+  class Products {
+    // DB stuff
+    private $conn;
+    private $table = 'Products';
+
+    // Post Properties
+    public $id;
+    public $category_id;
+    public $category_name;
+    public $title;
+    public $body;
+    public $author;
+    public $created_at;
+
+    // Constructor with DB
+    public function __construct($db) {
+      $this->conn = $db;
+    }
+
+    // Get Posts
+    public function read() {
+      // Create query
+      
+      $query = 'SELECT id, Name, Price, Time from ' . $this->table . 'ORDER BY
+                                  Price DESC';
+      
+
+      // Prepare statement
+      $stmt = $this->conn->prepare($query);
+
+      // Execute query
+      $stmt->execute();
+
+      return $stmt;
+    }
